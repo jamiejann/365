d8a63a78cb6b2fa3c035b901e743abb5a003b335
jamie <jamie.jann@gmail.com>
2018-07-29 15:56:43 -0700

    code words

--- 365A2HuffLZW/src/com/HuffLZW/HuffLZW.java
+++ 365A2HuffLZW/src/com/HuffLZW/HuffLZW.java
@@ -2,11 +2,24 @@ package com.HuffLZW;
 
 import javax.swing.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+
+class Node{
+    int data;
+    String x;
+    Node right = null;
+    Node left = null;
+}
 
 public class HuffLZW {
     private JButton button;
     private JPanel panel1;
     private JTextField textField;
+
+
 
     public HuffLZW(){
         button.addActionListener(e -> {
@@ -41,14 +54,93 @@ public class HuffLZW {
 
     private void runHuffman(byte[] data){
 
+        ArrayList<Byte> list= new ArrayList<>();
+        for(byte b: data){
+            list.add(b);
+        }
+        ArrayList<Integer> listInteger = new ArrayList<>();
+        for(byte b: data){
+            listInteger.add(Integer.valueOf(String.valueOf(b)));
+        }
+        System.out.println(listInteger);
+        ArrayList<Integer> unsignedIntList = new ArrayList<>();
+        System.out.println(" ");
+        for (byte audioByte : data) {
+            int temp = byteToUnsignedInt(audioByte);
+            unsignedIntList.add(temp);
+        }
+        System.out.println(unsignedIntList);
+
+        //Build Freq table
+        int freq[] = new int[unsignedIntList.size()];
+        for (int i : unsignedIntList) {
+            freq[unsignedIntList.get(i)] += 1;
+        }
+        /*
+        //printing
+        for(int i : freq){
+            System.out.println(i);
+        }
+        */
+        PriorityQueue<Node> pq = new PriorityQueue<>(unsignedIntList.size(), new huffComparator());
+        for(int i : freq) {
+            Node node = new Node();
+            node.x = String.valueOf(i);
+            node.data = freq[i];
+
+            pq.add(node);
+        }
+        Node root = null;
+
+        while(pq.size()>1){
+            Node x = pq.peek();
+            pq.poll();
+
+            Node y = pq.peek();
+            pq.poll();
+
+            Node newNode = new Node();
+            newNode.data = x.data + y.data;
+            newNode.x = "-";
+            newNode.left = x;
+            newNode.right = y;
+            root = newNode;
+            pq.add(newNode);
+
+        }
+
+        printCode(root, "");
+
+
     }
 
+    private static void printCode(Node root, String s){
+        ArrayList<String> compressed = new ArrayList<>();
+        if (root.left == null && root.right == null) {
+            //System.out.print(root.x  + ":" + s);
+            return;
+        }
+        printCode(root.left, s + "0");
+        compressed.add(s + "0");
+        printCode(root.right, s + "1");
+        compressed.add(s + "1");
+    }
+
+    private static int byteToUnsignedInt(byte b){
+        return 0x00 << 24 | b & 0xff;
+    }
 
     private static JFrame buildFrame(){
         JFrame frame = new JFrame();
         frame.setSize(704, 576);
         frame.setVisible(true);
         return frame;
+    }
+
+    class huffComparator implements Comparator<Node>{
+        public int compare(Node x, Node y){
+            return x.data - y.data;
+        }
     }
 
     public static void main(String[] args) {
