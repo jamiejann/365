cf69dfa184ecc28b799ade1f91b2ffde967b25e5
jamie <jamie.jann@gmail.com>
2018-07-29 14:37:51 -0700

    initial A2

---
+++ 365A2HuffLZW/src/com/HuffLZW/HuffLZW.java
@@ -0,0 +1,9 @@
+package com.HuffLZW;
+
+import javax.swing.*;
+
+public class HuffLZW {
+    private JButton button;
+    private JPanel panel1;
+    private JTextField textField;
+}
--- HuffLZW/src/com/HuffLZW/App.java
+++ HuffLZW/src/com/HuffLZW/App.java
@@ -1,8 +1,44 @@
 package com.HuffLZW;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 
 public class App {
     private JButton button;
     private JPanel panel1;
+
+    public App() {
+        button.addActionListener(e -> {
+            JFileChooser chooser = new JFileChooser();
+            chooser.showOpenDialog(null);
+            File f = chooser.getSelectedFile();
+            String fileName = f.getAbsolutePath();
+
+            try {
+                //TOOD: implement runnnable
+            } catch (IOException el){
+                el.printStackTrace();
+            }
+
+
+
+
+        });
+    }
+    public static JFrame buildFrame(){
+        JFrame frame = new JFrame();
+        frame.setSize(1000, 600);
+        frame.setVisible(true);
+        return frame
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
